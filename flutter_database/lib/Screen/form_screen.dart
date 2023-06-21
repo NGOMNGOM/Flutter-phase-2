1cc4a84ae6ba30493531050ed861e84ba556b433
NGOMNGOM <takongbaza@gmail.com>
2023-06-21 15:05:26 +0700

    Validation

--- flutter_database/lib/Screen/form_screen.dart
+++ flutter_database/lib/Screen/form_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 class FormScreen extends StatelessWidget {
-  const FormScreen({super.key});
+  FormScreen({super.key});
+  final formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -10,29 +11,47 @@ class FormScreen extends StatelessWidget {
           title: const Text("Second Page"),
         ),
         body: Form(
+            key: formKey,
             child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              TextFormField(
-                decoration: const InputDecoration(labelText: "First Input"),
-                autofocus: true,
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  TextFormField(
+                    decoration: const InputDecoration(labelText: "First Input"),
+                    autofocus: true,
+                    validator: (String? input) {
+                      if (input!.isEmpty) {
+                        return "Please input";
+                      }
+                      return null;
+                    },
+                  ),
+                  TextFormField(
+                      keyboardType: TextInputType.number,
+                      decoration:
+                          const InputDecoration(labelText: "Second Input"),
+                      validator: (String? input) {
+                        if (input!.isEmpty) {
+                          return "Please input";
+                        } else if (double.parse(input) <= 0) {
+                          return "Please input positive value";
+                        }
+                        return null;
+                      }),
+                  TextButton(
+                      style: TextButton.styleFrom(
+                          foregroundColor: Colors.blue,
+                          elevation: 5,
+                          backgroundColor: Colors.amber),
+                      onPressed: () {
+                        if (formKey.currentState!.validate()) {
+                          Navigator.pop(context);
+                        }
+                      },
+                      child: const Text("Submit"))
+                ],
               ),
-              TextFormField(
-                  keyboardType: TextInputType.number,
-                  decoration: const InputDecoration(labelText: "Second Input")),
-              TextButton(
-                  style: TextButton.styleFrom(
-                      foregroundColor: Colors.blue,
-                      elevation: 5,
-                      backgroundColor: Colors.amber),
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  child: const Text("Submit"))
-            ],
-          ),
-        )));
+            )));
   }
 }
